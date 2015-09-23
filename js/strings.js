f4b4d84ce44872ce10151d330a4620cca71be746
tejasgujjar <tejasgujjar@gmail.com>
2015-09-23 00:35:12 -0700

    added strings file

---
+++ js/strings.js
@@ -0,0 +1,64 @@
+username="Email";
+password="Password";
+userid_emtymsg = "Please enter E-mail ID";
+invalid_email_id = "Invalid E-mail ID";
+please_enter_password = 'Please enter password';
+error_title = "Error";
+ok_button_text = 'OK';
+network_disconnected_msg = "Network connectivity error.  Please check your connection and try again.";
+invalid_session_msg = "Session has expired.  Please log back in to continue.";
+login_failed_msg = "Invalid User Name/Password. Pelase try again.";
+shortDayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+shortMonthNames =['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+just_a_moment = "Just a moment...";
+/* Geofence and Notifications */
+home_text = "Home";
+geofence_text = "Geo Fence";
+notifications_text = "Notifications";
+create_new_geofence_text = "Create New Geo Fence";
+notifications_error_msg = "Error occurred when retrieving the notification history.";
+/*This Sections is for demo Data*/
+demo_notificationhistory_data='[{"notificationMsgId": "1207918", "notificationMessage": "A Drive Zone alert has been created for your Infiniti' + 
+'JX35. For more information, please visit infiniticonnection.com.", "notificationCreateDate": "2012-11-15T20:30:13+00:00",' +
+'"notificationUpdateDate": "2012-11-15T20:30:13+00:00", "serviceType": "DEMOGEOFENCE1"}, { "notificationMsgId": "1207959",' +
+'"notificationMessage": "A max speed alert of 30 MPH has been created for your Infiniti JX35 at 11/15/2012 02:45 PM CST.",' +
+'"notificationCreateDate": "2012-11-15T20:45:10+00:00", "notificationUpdateDate": "2012-11-15T20:45:10+00:00",' +
+'"serviceType": "DEMOSPEED_ALERT1", "notificationStatus": "SENT"}, { "notificationMsgId": "1208020",' +
+'"notificationMessage": "A max speed alert of 60 MPH has been created for your Infiniti JX35.", "notificationCreateDate":' + 
+'"2012-11-15T21:00:10+00:00", "notificationUpdateDate": "2012-11-15T21:00:10+00:00", "serviceType": "DEMOSPEED_ALERT2",' +
+'"notificationStatus": "SENT"}]';
+
+demo_geofence_data='{"geofences":[{"name":"Geofence1","center":"2 N Main St, Las Vegas, NV 89101","radius":"10.0",'+
+'"category":"Keep Out","activationTime":"2013-04-30T09:15:00+00:00","deactivationTime":"2013-05-01T09:45:00+00:00",'+
+'"status":"SUCCESS","serviceRequestId":"614c1231-5447-4672-9c3d-fcadd6f2cba6","fenceId":"13793","preferenceId":"13793",'+
+'"unit":"1"},{"name":"Geofence2","center":"1 N Main St, Las Vegas, NV 89101","radius":"6.0","category":"Stay Within",'+
+'"activationTime":null,"deactivationTime":null,"status":"{}","serviceRequestId":null,"fenceId":"13792",'+
+'"preferenceId":"13792","unit":"0"}]}';
+
+start_date_text = "Start Date";
+start_time_text = "Start Time";
+end_date_text = "End Date";
+end_time_text = "End Time";
+name_text = "Name";
+center_text = "Center";
+radius_text = "Radius";
+category_text = "Category";
+logout_text = "Logout";
+back_text = "Back";
+kmtext = " Km";
+miletext = " Mi";
+create_geofence_text = "Create Geo Fence";
+get_drivezone_list_error_txt = "Cannot retrieve Geo Fence list.  Please try again.";
+activate_drivezone_error_txt = "Cannot activate Geo Fence at this time.  Please try again.";
+deactivate_drivezone_error_txt = "Cannot deactivate Geo Fence at this time.  Please try again.";
+drivezone_list_empty = "No Geo Fences are available to activatate.";
+create_geofence_success_message="Geo Fence Created Successfully.";
+create_geofence_error_message="An error occired while creating Geo Fence. Pelase try again.";
+geoFence_duration_validation_text1 = "End time should be greater than start time.";
+geoFence_duration_validation_text2 = "Start Time should be greater than Current Time.";
+activate_geofence_done_txt = "Geo fence activated successfully.";
+activate_geofence_error_txt = "An error occured while activating geo fence please try again.";
+deactivate_geofence_done_txt = "Geo fence deactivated successfully.";
+deactivate_geofence_error_txt = "An error occured while deactivating geo fence please try again.";
+geofenceName_Null_errorMsg = "Please enter Geo Fence Name.";
+geoAddress_Null_errorMsg = "Please enter Address.";
